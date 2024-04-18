693d9bc75164aa68efefad09b0fb3e4b09616df1
huchka <huchka@me.com>
2024-04-18 17:06:14 +0900

    adding dummy purchase data.

--- config/mysql/init/init.sh
+++ config/mysql/init/init.sh
@@ -16,6 +16,13 @@ mysql -u root -ppassword sakila < "/docker-entrypoint-initdb.d/sakila/sakila-dat
 # small data for testing fast
 mysql -u root -ppassword sakila_small < "/docker-entrypoint-initdb.d/sakila/sakila-small.sql"
 
+# sample purchase data
+# create database
+mysql -uroot -ppassword -e "create database purchase"
+
+# import data
+mysql -u root -ppassword purchase < "/docker-entrypoint-initdb.d/purchase/purchase_data.sql"
+
 # show database
 echo "listing current databases.............."
 mysql -u root -ppassword -e "show databases"
